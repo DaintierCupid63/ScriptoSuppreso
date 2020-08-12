5c844a235cef79c597add73e5c0072655c677ae6
DaintierCupid63 <68474582+DaintierCupid63@users.noreply.github.com>
2020-08-12 12:24:35 -0500

    Create Ieatdogs.lua

---
+++ Ieatdogs.lua
@@ -0,0 +1,6 @@
+--[[https://v3rmillion.net/showthread.php?tid=942799, Universal Hitbox Expander by GameOverAgain on v3rmillion.
+]]
+loadstring(game:HttpGet("http://gameovers.net/Scripts/Free/HitboxExpander/main.lua", true))()
+wait(0.1)
+loadstring(game:HttpGet("http://scriptstream.xyz/ScriptStorage/ESPForArsenal.lua", true))()
+--Not mine cunt

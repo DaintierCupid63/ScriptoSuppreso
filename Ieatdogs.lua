5b161c5fa57b8b90be9319c3d343752e4815696d
DaintierCupid63 <68474582+DaintierCupid63@users.noreply.github.com>
2020-08-12 13:27:45 -0500

    Update Ieatdogs.lua

--- Ieatdogs.lua
+++ Ieatdogs.lua
@@ -3,5 +3,12 @@
 loadstring(game:HttpGet("http://gameovers.net/Scripts/Free/HitboxExpander/main.lua", true))()
 wait(0.1)
 loadstring(game:HttpGet("http://scriptstream.xyz/ScriptStorage/ESPForArsenal.lua", true))()
-game:GetService("StarterGui"):SetCore("ResetButtonCallback", true)
+--made by purgatory
+function onKeyPress(actionName, userInputState, inputObject)
+   if userInputState == Enum.UserInputState.Begin then
+       game.Players.LocalPlayer.Character:BreakJoints()
+   end
+end
+
+game.ContextActionService:BindAction("keyPress", onKeyPress, false, Enum.KeyCode.R) --change 'R' to another key if you want to change it
 --Not mine cunt
